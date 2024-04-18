ea061a99b4137a18a0a8be63534e3db41e4915bf
faraz9012 <54522532+faraz9012@users.noreply.github.com>
2024-04-18 11:52:12 +0500

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
+export const metadata: Metadata = {
+  title: "eMarketplace",
+  description: "We’re adding new features on a regular basis to make your experience fast, and easy.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
