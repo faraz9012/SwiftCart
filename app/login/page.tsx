265f07e4a0a7ef98f3232f6db36f0659e97c5cd5
faraz9012 <mfmushtaq.96@gmail.com>
2024-04-28 21:33:26 +0500

    [refac]: validation added in login form

---
+++ app/login/page.tsx
@@ -0,0 +1,31 @@
+import SignIn from "@/components/public/signin-form";
+import Image from "next/image";
+
+export default function Login() {
+  return (
+    <div className="w-full h-dvh lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <SignIn />
+      </div>
+      <div className="hidden bg-muted lg:block relative bg-black">
+        <Image
+          src="/images/login.webp"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+        <div className="absolute inset-0 bg-zinc-900 opacity-65"></div>
+        <div className="absolute top-8 p-4 text-white z-20 flex items-center text-2xl font-medium">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="mr-2 h-6 w-6"><path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3"></path></svg>Swift Cart 
+        </div>
+        <div className="absolute bottom-24 text-white z-20 p-4">
+          <blockquote className="space-y-2">
+            <p className="text-lg">“This library has saved me countless hours of work and helped me deliver stunning designs to my clients faster than ever before.”</p>
+            <footer className="text-sm">Sofia Davis</footer>
+          </blockquote>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ components/public/signin-form.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+    email: z.string().email({
+        message: "Not a valid email",
+    }),
+    password: z.string().min(1, {
+        message: "Password is required."
+    }),
+})
+
+export default function SignIn() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: ""
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+    return (
+        <>
+            <div className="mx-auto grid p-4 gap-6">
+                <div className="grid gap-2 text-center">
+                    <h1 className="text-3xl font-bold">Login</h1>
+                    <p className="text-balance text-muted-foreground">
+                        Enter your email below to login to your account
+                    </p>
+                </div>
+
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                        <div className="grid gap-4">
+                            <div className="grid gap-2">
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Email</FormLabel>
+                                            <FormControl>
+                                                <Input type="email" placeholder="admin@yourstore.com" {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                    <Link
+                                        href="/forgot-password"
+                                        className="ml-auto inline-block text-sm underline"
+                                    >
+                                        Forgot your password?
+                                    </Link>
+
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Password</FormLabel>
+                                            <FormControl>
+                                                <Input type="password" />
+                                            </FormControl>
+                                            <FormMessage />
+                                            
+                                        </FormItem>
+                                        
+                                    )}
+                                />
+                            </div>
+                            <Button type="submit" className="w-full">
+                                Login
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+                <div className="mt-4 text-center text-sm">
+                    Don&apos;t have an account?{" "}
+                    <Link href="#" className="underline">
+                        Sign up
+                    </Link>
+                </div>
+            </div>
+        </>
+    )
+}
